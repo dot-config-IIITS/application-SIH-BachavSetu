79723a85ce37c263e671a8511499a749bb91658f
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2023-12-14 19:52:18 +0530

    Added Feedback Page

--- bachavsetu/lib/main.dart
+++ bachavsetu/lib/main.dart
@@ -4,8 +4,6 @@ import 'init_page.dart';
 import 'login/welcome.dart';
 import 'article/articlehomepage.dart';
 import 'settings/mock_drill.dart';
-import '/settings/feedback.dart';
-import 'package:bachavsetu/login/form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,12 +18,10 @@ class MyApp extends StatelessWidget {
       title: 'BachavSetu',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'GE Inspira'),
-      // home: Welcome(),
+      home: Welcome(),
       // home: ArticleHomePage(),
       // home: InitPage(),
       // home: MockDrill(),
-      // home: FeedbackForm(),
-      home: FormPage(),
     );
   }
 }
---
+++ bachavsetu/lib/settings/feedback.dart
@@ -0,0 +1,123 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class FeedbackForm extends StatefulWidget {
+  const FeedbackForm({Key? key}) : super(key: key);
+
+  @override
+  _FeedbackFormState createState() => _FeedbackFormState();
+}
+
+class _FeedbackFormState extends State<FeedbackForm> {
+  late List<String> stateNames;
+  Map<String, List<String>> districts = {};
+  late String selectedStateName;
+  late String selectedDistrict;
+  String feedback = '';
+
+  @override
+  void initState() {
+    super.initState();
+    selectedStateName = '';
+    selectedDistrict = '';
+    stateNames = [];
+    _loadStatesAndDistricts();
+  }
+
+  Future<void> _loadStatesAndDistricts() async {
+    final String data = await rootBundle
+        .loadString('assets/settingsAssets/states_districts.json');
+    final Map<String, dynamic> statesData = json.decode(data);
+
+    statesData.forEach((key, value) {
+      stateNames.add(value['name']);
+      districts[value['name']] = List<String>.from(value['districts']);
+    });
+
+    setState(() {
+      selectedStateName = stateNames.isNotEmpty ? stateNames[0] : '';
+      selectedDistrict = districts[selectedStateName]!.isNotEmpty
+          ? districts[selectedStateName]![0]
+          : '';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Feedback Form'),
+        backgroundColor: Colors.deepPurple.shade50,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            DropdownButtonFormField<String>(
+              decoration: const InputDecoration(labelText: 'Select State'),
+              value: selectedStateName,
+              items: stateNames.map((stateName) {
+                return DropdownMenuItem<String>(
+                  value: stateName,
+                  child: Text(stateName),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedStateName = value!;
+                  selectedDistrict =
+                      districts[value]!.isNotEmpty ? districts[value]![0] : '';
+                });
+              },
+            ),
+            const SizedBox(height: 16.0),
+            DropdownButtonFormField<String>(
+              decoration: const InputDecoration(labelText: 'Select District'),
+              value: selectedDistrict,
+              items: districts[selectedStateName]?.map((district) {
+                    return DropdownMenuItem<String>(
+                      value: district,
+                      child: Text(district),
+                    );
+                  }).toList() ??
+                  [],
+              onChanged: (value) {
+                setState(() {
+                  selectedDistrict = value!;
+                });
+              },
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Enter Your Feedback Here',
+                hintText: 'Enter Your Feedback Here',
+                alignLabelWithHint: true,
+                border: OutlineInputBorder(),
+              ),
+              maxLines: 4,
+              onChanged: (value) {
+                setState(() {
+                  feedback = value;
+                });
+              },
+            ),
+            const SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: () {
+                // FEEDBACK LOGIC TO BE IMPLEMENTED
+
+                print(
+                    'State: $selectedStateName, District: $selectedDistrict, Feedback: $feedback');
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
