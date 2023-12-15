c4c5bd8326b4cf5308a6d86d75432a07233640b4
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2023-12-15 19:28:26 +0530

    Contact Us

--- bachavsetu/lib/settings/contactus/contact_us.dart
+++ bachavsetu/lib/settings/contactus/contact_us.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import 'contact_card.dart';
 
 class ContactUsPage extends StatelessWidget {
   final List<ContactCard> developers = [
     ContactCard(
-      name: 'John Doe',
+      name: 'Vinayak Anand',
       role: 'Flutter Developer',
-      imageUrl: 'https://example.com/john_doe_image.jpg',
-      githubUrl: 'https://github.com/johndoe',
-      linkedinUrl: 'https://www.linkedin.com/in/johndoe/',
+      githubUrl: 'https://github.com/Vinayak-Anand',
+      linkedinUrl: 'https://www.linkedin.com/in/vinayak-anand/',
+    ),
+    ContactCard(
+      name: 'Hruthik Chitti',
+      role: 'Backend Developer',
+      githubUrl: 'https://github.com/Hruthik0x',
+      linkedinUrl:
+          'https://www.linkedin.com/in/hruthik-chitti-0827ab276/?originalSubdomain=in',
+    ),
+    ContactCard(
+      name: 'Suvan Sarkar',
+      role: 'Backend Developer',
+      githubUrl: 'https://github.com/Suvansarkar',
+      linkedinUrl: 'https://www.linkedin.com/in/suvan-sarkar-b1438017a/',
+    ),
+    ContactCard(
+      name: 'Bishwajeet Sahoo',
+      role: 'Flutter Developer',
+      githubUrl: 'https://github.com/SahooBishwajeet',
+      linkedinUrl: 'https://www.linkedin.com/in/bishwajeet-sahoo/',
+    ),
+    ContactCard(
+      name: 'Shrishti Mahor',
+      role: 'UI/UX Designer',
+      githubUrl: 'https://github.com/shrishti0308',
+      linkedinUrl: 'https://www.linkedin.com/in/shrishti-mahor/',
+    ),
+    ContactCard(
+      name: 'Piyush Singh',
+      role: 'Flutter Developer',
+      githubUrl: 'https://github.com/piyush-bug',
+      linkedinUrl: 'https://www.linkedin.com/in/piyush-bug/',
+    ),
+    ContactCard(
+      name: 'Saurabh Pal',
+      role: 'Backend Developer',
+      githubUrl: 'https://github.com/virinci',
+      linkedinUrl: 'https://www.linkedin.com/in/virinci/',
     ),
   ];
 
@@ -31,33 +70,15 @@ class ContactUsPage extends StatelessWidget {
   }
 }
 
-class ContactCard {
-  final String name;
-  final String role;
-  final String imageUrl;
-  final String githubUrl;
-  final String linkedinUrl;
-
-  ContactCard({
-    required this.name,
-    required this.role,
-    required this.imageUrl,
-    required this.githubUrl,
-    required this.linkedinUrl,
-  });
-}
-
 class ContactCardWidget extends StatelessWidget {
   final ContactCard contactCard;
 
   const ContactCardWidget({Key? key, required this.contactCard})
       : super(key: key);
 
-  void _launchURL(String url) async {
-    if (await canLaunchUrl(url as Uri)) {
-      await launchUrl(url as Uri);
-    } else {
-      throw 'Could not launch $url';
+  Future<void> _launchUrl(Uri _url) async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
     }
   }
 
@@ -65,53 +86,60 @@ class ContactCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 4.0,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
-            contactCard.imageUrl,
-            height: 150.0,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  contactCard.name,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 16.0),
-                ),
-                const SizedBox(height: 4.0),
-                Text(contactCard.role),
-                const SizedBox(height: 8.0),
-                Row(
-                  children: [
-                    InkWell(
-                      onTap: () => _launchURL(contactCard.githubUrl),
-                      child: Image.asset(
-                        'assets/github_logo.png', // Replace with your GitHub logo asset
-                        height: 24.0,
-                        width: 24.0,
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    InkWell(
-                      onTap: () => _launchURL(contactCard.linkedinUrl),
-                      child: Image.asset(
-                        'assets/linkedin_logo.png', // Replace with your LinkedIn logo asset
-                        height: 24.0,
-                        width: 24.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              'assets/settingsAssets/avatar.png',
+              height: 120.0,
+              width: 120.0,
+              fit: BoxFit.cover,
             ),
-          ),
-        ],
+            const SizedBox(width: 16.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    contactCard.name,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 22.0),
+                  ),
+                  const SizedBox(height: 4.0),
+                  Text(
+                    contactCard.role,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w300, fontSize: 16.0),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () =>
+                            _launchUrl(Uri.parse(contactCard.githubUrl)),
+                        child: const Icon(
+                          Ionicons.logo_github,
+                          size: 24.0,
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      InkWell(
+                        onTap: () =>
+                            _launchUrl(Uri.parse(contactCard.linkedinUrl)),
+                        child: const Icon(
+                          Ionicons.logo_linkedin,
+                          size: 24.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
